1dd0c07722e04e5999b1c1c1e83a48673e354bf6
umerc21061 <umerc21061@gmail.com>
2024-04-26 12:12:47 +0500

    update

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -49,6 +49,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR,'reactfrontend/build')],
+        
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'reactfrontend/build/static')]
 
--- djangobackend/views.py
+++ djangobackend/views.py
@@ -115,68 +115,6 @@ def get_crew_data(request):
         return JsonResponse({'error': str(e)})
 
 
-
-from django.http import JsonResponse
-from pymongo import MongoClient
-
-def get_portfolio_data(request):
-    try:
-        # Connect to MongoDB
-        client = MongoClient("mongodb+srv://umer:umer123456@cluster0.chseyyo.mongodb.net/")
-
-        # Access the portfolio database
-        db = client.portfolio
-
-        # Access the project2 collection
-        collection = db.project2
-
-        # Fetch data from MongoDB
-        data = collection.find({}, {'_id': 0, 'category': 1, 'urls': 1, 'video_text': 1})
-
-        # Convert MongoDB cursor to list of dictionaries
-        portfolio_data = list(data)
-
-        # Close MongoDB connection
-        client.close()
-
-        return JsonResponse(portfolio_data, safe=False)
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
-
-
-
-    try:
-        # Connect to MongoDB
-        client = MongoClient("mongodb+srv://umer:umer123456@cluster0.chseyyo.mongodb.net/")
-
-        # Access the portfolio database
-        db = client.portfolio
-
-        # Access the project collection
-        collection = db.project
-
-        # Fetch data from MongoDB
-        data = collection.find({}, {'_id': 0, 'portfolioproject_url.expertise': 1, 'portfolioproject_url.industry': 1})
-
-        # Convert MongoDB cursor to list of dictionaries
-        portfolio_data = list(data)
-
-        # Close MongoDB connection
-        client.close()
-
-        return JsonResponse(portfolio_data, safe=False)
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
-    
-
-
-
-
-    # views.py
-
-
-
-
 def get_job_data(request):
     # Connect to MongoDB
     client = MongoClient("mongodb+srv://umer:umer123456@cluster0.chseyyo.mongodb.net/")
@@ -260,8 +198,6 @@ def blogpage2(request):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
 
-from django.http import JsonResponse
-from pymongo import MongoClient
 
 def get_reward_data(request):
     try:
