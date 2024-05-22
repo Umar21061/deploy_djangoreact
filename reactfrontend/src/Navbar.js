dba4c6772946c925bbde3d9ba30a6d6ed365a441
umerc21061 <umerc21061@gmail.com>
2024-05-22 17:36:14 +0500

    update

--- reactfrontend/src/Navbar.js
+++ reactfrontend/src/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom'; // Import Link along with NavLink
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './image/logosh.png';
-import './Home.css';
+import './Navbar.css';
 
 const Navbar = () => {
     const [expanded, setExpanded] = useState(false);
