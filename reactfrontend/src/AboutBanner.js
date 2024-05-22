cb68967e801fac6129610a7bd1d6e4f3e67d3c50
umerc21061 <umerc21061@gmail.com>
2024-05-23 00:42:20 +0500

    update

--- reactfrontend/src/AboutBanner.js
+++ reactfrontend/src/AboutBanner.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './AboutBanner.css';
 import team from './image/team.jpg';
 
 const AboutBanner = () => {
   return (
-    <div style={{ height: '60vh', position: 'relative' }}>
-      <img src={team} alt="Team" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: 'white' }}>
-        <h1 style={{ fontSize: '3rem', fontWeight: 'bold', lineHeight: '1.2' }}>
+    <div className="about-banner-container">
+      <img src={team} alt="Team" className="about-banner-image" />
+      <div className="about-banner-text">
+        <h1 className="about-banner-heading">
           Where passion meets purpose, and every journey is a chapter in the narrative of our collective vision.
         </h1>
       </div>
